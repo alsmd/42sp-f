c477270d36b5cdc7e5d6910822f26b0d4997c573
alsmd <dangerveneta@gmail.com>
2021-08-05 16:15:27 -0300

    fix

--- projetos/libft/ft_putendl_fd.c
+++ projetos/libft/ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: flavio <flavio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 14:20:26 by flavio            #+#    #+#             */
-/*   Updated: 2021/08/05 16:13:16 by flavio           ###   ########.fr       */
+/*   Updated: 2021/08/05 16:14:59 by flavio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	ft_putendl_fd(char *s, int fd)
 
 	
 	index = 0;
-	if (!s)
+	if (s)
 	{
 		while (s[index])
 		{
